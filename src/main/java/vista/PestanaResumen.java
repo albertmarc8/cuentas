a1f9148707b6010411fb5ae0431b57f7f79b7470
Albert Marc <albertmarc8@gmail.com>
2020-05-08 20:17:40 +0200

    Commit inicial de la PestañaResumen

---
+++ src/main/java/vista/PestanaResumen.java
@@ -0,0 +1,44 @@
+package vista;
+
+import controlador.Controlador;
+import modelo.Modelo;
+
+import javax.swing.*;
+import java.awt.*;
+import java.math.BigDecimal;
+
+public class PestanaResumen extends JPanel {
+    private Controlador mControlador;
+    private Modelo mModelo;
+
+    private JLabel total;
+    private JLabel cuota;
+
+    public PestanaResumen(Controlador controlador, Modelo modelo) {
+        this.mControlador = controlador;
+        this.mModelo = modelo;
+
+        this.setLayout(new BorderLayout());
+        JPanel superior = new JPanel();
+        superior.setLayout(new BoxLayout(superior, BoxLayout.PAGE_AXIS));
+        total = new JLabel("Total: 0€");
+        superior.add(total);
+        cuota = new JLabel("Cuota: 0€");
+        superior.add(cuota);
+        add(superior, BorderLayout.PAGE_START);
+
+        JTable tabla = new JTable(mModelo.getTableModel());
+        JScrollPane scrollPane = new JScrollPane(tabla);
+        add(scrollPane, BorderLayout.CENTER);
+    }
+
+    public void setCuota(BigDecimal cuota) {
+        this.cuota.setText("Cuota: " + cuota + "€");
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total.setText("Cuota: " + total + "€");
+    }
+
+
+}
--- src/main/java/vista/VistaCuentas.java
+++ src/main/java/vista/VistaCuentas.java
@@ -5,6 +5,7 @@ import modelo.Modelo;
 
 import javax.swing.*;
 import java.awt.*;
+import java.math.BigDecimal;
 
 public class VistaCuentas implements Vista {
 
@@ -12,73 +13,28 @@ public class VistaCuentas implements Vista {
     private Controlador controlador;
 
     private JFrame ventana;
-    private JLabel total;
-    private JLabel cuota;
 
     private PestanaPago pestañaPago;
+    private PestanaResumen pestañaResumen;
 
     private JTextArea pagosARealizar;
-
-    public void setControlador(Controlador controlador) {
-        this.controlador = controlador;
-    }
-
-    public void setModelo(Modelo modelo) {
-        this.modelo = modelo;
-    }
 
     public void crearGUI() {
         ventana = new JFrame("cuentas");
 
         pestañaPago = new PestanaPago(controlador);
-        JPanel resumen = crearPestanyaResumen();
+        pestañaResumen = new PestanaResumen(controlador, modelo);
         JPanel realizarPagos = crearPestanyaRealizarPagos();
 
         JTabbedPane tabs = new JTabbedPane();
         tabs.add("Pago", pestañaPago);
-        tabs.add("Resumen", resumen);
+        tabs.add("Resumen", pestañaResumen);
         tabs.add("Pagos a realizar", realizarPagos);
 
         ventana.add(tabs);
         ventana.pack();
         ventana.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         ventana.setVisible(true);
-    }
-
-
-    @Override
-    public String getNombre() {
-        return pestañaPago.getNombre();
-    }
-
-    @Override
-    public String getConcepto() {
-        return pestañaPago.getConcepto();
-    }
-
-    @Override
-    public String getCantidad() {
-        return pestañaPago.getCantidad();
-    }
-
-    @Override
-    public void setConcepto(String s) {
-        pestañaPago.setConcepto(s);
-    }
-
-    @Override
-    public void setCantidad(String s) {
-        pestañaPago.setCantidad(s);
-    }
-
-    @Override
-    public void mensajeError(String mensaje) {
-        JOptionPane.showMessageDialog(ventana, mensaje, "Error", JOptionPane.ERROR_MESSAGE);
-    }
-
-    @Override
-    public void focoEnNombre() {
-        pestañaPago.focoEnNombre();
     }
 
     private JPanel crearPestanyaRealizarPagos() {
@@ -96,34 +52,57 @@ public class VistaCuentas implements Vista {
         return panel;
     }
 
-
-    private JPanel crearPestanyaResumen() {
-        JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout());
-
-        JPanel superior = new JPanel();
-        superior.setLayout(new BoxLayout(superior, BoxLayout.PAGE_AXIS));
-        total = new JLabel("Total: 0€");
-        superior.add(total);
-        cuota = new JLabel("Cuota: 0€");
-        superior.add(cuota);
-        panel.add(superior, BorderLayout.PAGE_START);
-
-        JTable tabla = new JTable(modelo.getModelo());
-        JScrollPane scrollPane = new JScrollPane(tabla);
-        panel.add(scrollPane, BorderLayout.CENTER);
-
-        return panel;
+    @Override
+    public void datosCambiados() {
+        modelo.getTableModel().fireTableDataChanged();
+        pestañaResumen.setCuota(modelo.getCuota());
+        pestañaResumen.setTotal(modelo.getTotal());
     }
 
     @Override
-    public void datosCambiados() {
-        modelo.getModelo().fireTableDataChanged();
-        total.setText("Total: " + modelo.getTotal() + "€");
-        cuota.setText("Cuota: " + modelo.getCuota() + "€");
+    public void focoEnNombre() {
+        pestañaPago.focoEnNombre();
+    }
+
+    @Override
+    public String getCantidad() {
+        return pestañaPago.getCantidad();
+    }
+
+    @Override
+    public String getConcepto() {
+        return pestañaPago.getConcepto();
+    }
+
+    @Override
+    public String getNombre() {
+        return pestañaPago.getNombre();
+    }
+
+    @Override
+    public void mensajeError(String mensaje) {
+        JOptionPane.showMessageDialog(ventana, mensaje, "Error", JOptionPane.ERROR_MESSAGE);
     }
 
     public void ponerMensaje(String s) {
         pagosARealizar.setText(s);
+    }
+
+    @Override
+    public void setCantidad(String s) {
+        pestañaPago.setCantidad(s);
+    }
+
+    @Override
+    public void setConcepto(String s) {
+        pestañaPago.setConcepto(s);
+    }
+
+    public void setControlador(Controlador controlador) {
+        this.controlador = controlador;
+    }
+
+    public void setModelo(Modelo modelo) {
+        this.modelo = modelo;
     }
 }
